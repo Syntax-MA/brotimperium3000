ecb1f71eda87af1eb07eb01d033ce04bdd643785
Syntax-MA <mete.ardic@syntax-institut.de>
2026-02-12 11:38:24 +0100

    md file with git commands

---
+++ git_befehle.sh
@@ -0,0 +1,11 @@
+
+
+git branch # übersicht über branches
+
+git branch <name> # neuen Branch erstellen aber auf altem bleiben
+git checkout <name> # wechselt zum Branch <name>
+
+git checkout -b <name> # erstellt und wechselt direkt auf den neuen Branch
+
+
+
